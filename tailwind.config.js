bca02fdea41e89e5573dbe6ebf10e832436bbf91
Axxie <d3896372@live.tees.ac.uk>
2025-01-04 04:49:10 +0530

    Chs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
-  safelist: [
-    // Add classes you suspect might be purged unnecessarily
-    'bg-red-500',
-    'text-center',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
